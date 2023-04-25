ef705ae702b6962ad7bae50e9470e9ba248fcc1f
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-04-25 22:57:14 +0300

    Create branch & update Main file

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ hello world
 class Main {
     public static void main(String[] args)
     {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // Homework 2 Branch
         System.out.println("Hello and welcome to TeacMeSkills course");
 
         System.out.print("");
