2caca97e8b6db09b7aaacc1b9813c6d8f62ec075
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-04-30 17:41:37 +0400

    Added code in Homework 2 branch which returns Christmas tree & grid in terminal

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ hello world
 class Main {
     public static void main(String[] args)
     {
-        // Homework 2 Branch
+        /* Homework 2 Branch
         System.out.println("Hello and welcome to TeacMeSkills course");
 
         System.out.print("");
@@ -24,10 +24,47 @@ class Main {
                 ". Welcome to my First " +
                 "Lesson to...........");
 
-        // This is an example comment for Git
+        //This is an example comment for Git
         System.out.println();
         System.out.print("Please, enter your name & surname: ");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
+        */
+
+        //Homework 2. This code returns Christmas tree
+        System.out.println("       *       ");
+        System.out.println("      ***      ");
+        System.out.println("     *****     ");
+        System.out.println("    *******    ");
+        System.out.println("   *********   ");
+        System.out.println();
+
+        //Homework 2. This code returns grid
+        System.out.println("      |    |       ");
+        System.out.println("   ___|____|____   ");
+        System.out.println("      |    |       ");
+        System.out.println("   ___|____|____   ");
+        System.out.println("      |    |       ");
+        System.out.println("      |    |       ");
+
+        /*This code returns Christmas tree (realization was found by googling in internet, just for self-education)
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of tree : ");
+        int h = sc.nextInt();
+
+        for(int i = 1;i <= 6;i++){
+
+            for(int j = h-i;j > 0;j--){
+                System.out.print(" ");
+            }
+
+            for(int k = 1;k <= i;k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+         */
     }
 }
