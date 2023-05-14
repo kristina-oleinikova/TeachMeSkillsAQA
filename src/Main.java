408cdb75daa41431d949e90d876bb247de974847
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-05-14 23:48:37 +0400

    Added homework 5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,110 @@
-/*
-hello world
- */
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.Math;
+
 class Main {
-    public static void main(String[] args)
-    {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome to TeacMeSkills course");
+    public static void main (String[] args) {
+
+        arrayContainsNumber();
+        createArray();
+        createNewArray();
+    }
+
+
+    public static void arrayContainsNumber(){
+        int[] num = new int []{1, 2, 3, 8, 15, 25, 99};
+
+        Scanner enter = new Scanner (System.in);
+        System.out.print("Enter digit number: ");
+        int number = enter.nextInt();
+
+        for(int index = 0; index < num.length; index++){
+            if (number == num[index]) {
+                System.out.println("Entered number is in Array");
+                return;
+                }
+        }
+        System.out.println("There is not such number in Array");
+    }
+
+
+    public static void createArray() {
+        int[] arr = new int[]{1,2,3,4,5};
+        int num = 0;
+
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Please, enter any digit number: ");
+        int number = enter.nextInt();
+
+        for(int i = 0; i< arr.length; i++){
+            if(number != arr[i]){
+                num++;
+            }
+        }
+
+        int[] copyArr = new int[num];
+
+        int offset = 0;
+        boolean isNumberInArray = false;
+
+        for(int i = 0; i < arr.length; i++){
+            if(number == arr[i]){
+                offset++;
+                isNumberInArray = true;
+            }else{
+                copyArr[i-offset] = arr[i];
+            }
+        }
+
+        for(int i = 0; i< copyArr.length; i++){
+            System.out.print(copyArr[i] + " ");
+        }
+            System.out.println();
+
+        if (isNumberInArray == true){
+            System.out.print("Congrats! The entered number is in Array!");
+        } else{
+            System.out.print("There is no such number in Array");
+        }
+    }
+
+    public static void createNewArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please, enter length of new Array: ");
+        int lengthOfArray = input.nextInt();
+
+        double insertNumber = Math.random();
+
+        double [] newArray = new double[lengthOfArray];
+
+        System.out.print(newArray.length);
+
+        for(int i = 0; i < lengthOfArray; i++){
+            newArray[i] = insertNumber;
+            System.out.print(newArray[i]+ " ");
+        }
+
+        System.out.println();
+
+
+        double maxNumber = newArray[0];
+        double minNumber = newArray [0];
+        double sumOfNumbers = 0;
+        double averageNumber = 0;
+
+        for (int i = 0; i<newArray.length; i++){
+            if(newArray[1] < minNumber){
+                minNumber = newArray[i];
+            }
+            if(newArray[1] > maxNumber){
+                maxNumber = newArray[i];
+            }
+            sumOfNumbers =+ newArray[i];
+            averageNumber = sumOfNumbers / lengthOfArray;
+        }
+
+        System.out.println(maxNumber + " is the biggest number in Array");
+        System.out.println(minNumber + " is the smallest number in Array");
+        System.out.println(averageNumber + " is the average of numbers from Array");
     }
 }
