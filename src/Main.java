59cfe2e64d6ddb10330a6aeff179f911c5646aa4
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-05-15 14:44:42 +0400

    Added updates

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.lang.Math;
 class Main {
     public static void main (String[] args) {
 
-//        arrayContainsNumber();
-//        createArray();
-//        createNewArray();
+        arrayContainsNumber();
+        createArray();
+        createNewArray();
         createTwoDifferentArrays();
     }
 
