7d1899e9dcf344c74f7e6224553fc897f0309d87
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-05-19 00:41:43 +0400

    Added first part of homework

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-/*
-hello world
- */
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args)
     {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome to TeacMeSkills course");
+        increaseEachNumberInArray();
+    }
+
+    public static void increaseEachNumberInArray(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please, enter digit number to increase each element in array: ");
+        int number = input.nextInt();
+
+        int [][][] array = new int [][][]{
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9, 2}
+                }
+        };
+
+        for(int l1 = 0; l1 < array.length; l1++){
+            for(int l2 = 0; l2 < array[l1].length; l2++){
+                for(int l3 = 0; l3 < array[l1][l2].length; l3++){
+                    array[l3] = array[l3] + number;
+                    System.out.println(array[l1][l2][l3]+ " ");
+                }
+            }
+        }
     }
 }
