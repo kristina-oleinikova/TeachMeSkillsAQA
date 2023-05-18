09790b67d6b66fa7b56a1e2a4d02a1b6bc49cd16
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-05-19 01:42:40 +0400

    Added changes

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,61 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args)
     {
-        increaseEachNumberInArray();
+        printRandomTrippleArray();
     }
 
-    public static void increaseEachNumberInArray(){
+    public static int getNumberFromConsole(){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please, enter digit number to increase each element in array: ");
-        int number = input.nextInt();
+        System.out.print("Please, enter digit number to increase each element in array: ");
+        return input.nextInt();
+    }
 
-        int [][][] array = new int [][][]{
-                {
-                        {1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9, 2}
-                }
-        };
+    public static int[][][] createTripleArray(){
+        int [][][] array = new int [3][3][3];
 
         for(int l1 = 0; l1 < array.length; l1++){
             for(int l2 = 0; l2 < array[l1].length; l2++){
                 for(int l3 = 0; l3 < array[l1][l2].length; l3++){
-                    array[l3] = array[l3] + number;
-                    System.out.println(array[l1][l2][l3]+ " ");
+                    array[l1][l2][l3] = (int) (Math.random()*10) +1;
                 }
             }
+        }
+
+        return array;
+    }
+
+    public static void printRandomTrippleArray(){
+        int number = getNumberFromConsole();
+        int [][][] array = createTripleArray();
+        int [][][] finalTrippleArray = increaseEachNumberInArray(array, number);
+
+        printTripleArrayInConsole(finalTrippleArray);
+    }
+
+    public static int[][][] increaseEachNumberInArray(int [][][] array, int number){
+
+        for(int l1 = 0; l1 < array.length; l1++){
+            for(int l2 = 0; l2 < array[l1].length; l2++){
+                for(int l3 = 0; l3 < array[l1][l2].length; l3++){
+                    array[l1][l2][l3] = array[l1][l2][l3] + number;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void printTripleArrayInConsole(int[][][] trippleArray){
+
+        for(int i = 0; i < trippleArray.length; i++){
+            for(int k = 0; k < trippleArray[i].length; k++){
+                for(int j = 0; j < trippleArray[i][k].length; j++){
+                    System.out.print(trippleArray[i][k][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
         }
     }
 }
